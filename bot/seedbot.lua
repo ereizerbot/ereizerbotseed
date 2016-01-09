334a7c4640b305a4f7ad37e8b294d284237c3ac0
ereizerbot <mohammadereizer@gmail.com>
2016-01-09 13:43:03 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -228,7 +228,7 @@ function create_config( )
     about_text = [[Teleseed v1
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
+https://github.com/ereizerbot/ereizerbotseed
 
 Admins
 @djmjcj 
