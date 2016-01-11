aa5fee0262bb546e9f004cac1dab6ec236f1e489
ereizerbot <mohammadereizer@gmail.com>
2016-01-11 17:54:34 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {95822747,139837032,0,tonumber(our_id)},--Sudo users
+    sudo_users = {139837032,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {data = 'data/moderation.json'},--Realms Id
     moderation = {data = 'data/moderation.json'},
